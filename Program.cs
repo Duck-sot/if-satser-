fb8bc42804a5cf22082bd5af0265cf6ac22de9cd
Duck-sot <raja07001@utb.vaxjo.se>
2024-08-30 13:53:03 +0200

    Uppgifter färdiga
    färdig med de tre uppgifterna som vi fick 24/08/30 på programering 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("How is the whether");
+string respons = Console.ReadLine();
+if(respons == "Good" || respons == "good" ){
+    Console.WriteLine("Let's go for a walk");
+}
+else if(respons == "bad" || respons == "Bad"){
+    Console.WriteLine("let's stay inside and play mario cart");
+}
+else{
+    Console.WriteLine("I do not understand");
+}
